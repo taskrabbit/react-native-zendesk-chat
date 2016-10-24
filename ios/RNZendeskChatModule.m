359cbdbda0740099facb142f9659eef77b2a93d1
Jordan Smith <jtsu888@gmail.com>
2016-10-24 14:16:00 -0700

    department fix

--- ios/RNZendeskChatModule.m
+++ ios/RNZendeskChatModule.m
@@ -42,7 +42,7 @@ RCT_EXPORT_METHOD(startChat:(NSDictionary *)options) {
       config.preChatDataRequirements.name       = ZDCPreChatDataRequired;
       config.preChatDataRequirements.email      = ZDCPreChatDataRequired;
       config.preChatDataRequirements.phone      = ZDCPreChatDataRequired;
-      config.preChatDataRequirements.department = ZDCPreChatDataRequired;
+      config.preChatDataRequirements.department = ZDCPreChatDataRequiredEditable;
       config.preChatDataRequirements.message    = ZDCPreChatDataRequired;
     }];
   });
