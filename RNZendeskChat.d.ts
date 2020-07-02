a76e3e5b0856429e5e13d8dad170479838c93a3f
Frederic Barthelemy <git@fbartho.com>
2020-07-02 16:45:13 -0700

    ZendeskChatSDK V2 - iOS WIP

--- RNZendeskChat.d.ts
+++ RNZendeskChat.d.ts
@@ -3,29 +3,71 @@ declare module "react-native-zendesk-chat" {
 		name?: string;
 		email?: string;
 		phone?: string;
-
-		/** only implemented on iOS */
-		shouldPersist?: boolean;
 	}
-	interface StartChatOptions_iOS extends VisitorInfoOptions {
+
+	interface MessagingOptions_iOS {
+		/** Set this to set the bot's displayName */
+		botName?: string;
+		/** Will be loaded using [UIImage imageWithName:…] ) */
+		botImageName?: string;
+		/** Will be loaded using [UIImage imageWithUrl:…] */
+		botImageUrl?: string;
+	}
+
+	const enum PreChatFormFieldOptionVisibility {
+		hidden = "hidden",
+		optional = "optional",
+		required = "required",
+	}
+
+	interface StartChatOptions extends VisitorInfoOptions {
 		department?: string;
 		tags?: string[];
 
-		// Flags to disable some fields collected by default.
-		emailNotRequired?: boolean;
-		phoneNotRequired?: boolean;
-		departmentNotRequired?: boolean;
-		messageNotRequired?: boolean;
+		behaviorFlags?: {
+			/** if omitted, the form is enabled */
+			showPreChatForm?: boolean;
+			/** if omitted, the prompt is enabled */
+			showChatTranscriptPrompt?: boolean;
+			/** if omitted, the form is enabled */
+			showOfflineForm?: boolean;
+			/** if omitted, the agent availability message is enabled */
+			showAgentAvailability?: boolean;
+		};
+
+		/** If omitted, the preChatForm will be left as default in Zendesk */
+		preChatFormOptions?: {
+			/** Should we ask the user for a contact email? */
+			email?: PreChatFormFieldOptionVisibility;
+			/** Should we ask the user their name? */
+			name?: PreChatFormFieldOptionVisibility;
+			/** Should we ask the user for their phone number? */
+			phoneNumber?: PreChatFormFieldOptionVisibility;
+			/** Should we ask the user which department? */
+			department?: PreChatFormFieldOptionVisibility;
+		};
+
+		/**
+		 * Configure the Chat-Bot (if any)
+		 */
+		messagingOptions?: MessagingOptions_iOS;
+
+		/**
+		 * If not provided, this will be "Close" -- not localized!
+		 */
+		localizedDismissButtonTitle?: string;
 	}
-	type StartChatOptions_Android = VisitorInfoOptions;
-	type StartChatOptions = StartChatOptions_iOS & StartChatOptions_Android;
 
 	class RNZendeskChatModuleImpl {
 		init: (zendeskAccountKey: string) => void;
 
-		setVisitorInfo: (options: VisitorInfoOptions) => void;
-
 		startChat: (options: StartChatOptions) => void;
+		/**
+		 * Backwards Compatibility!
+		 * - You can pass all these parameters to RNZendeskChatModule.startChat
+		 * - So you should probably prefer that method
+		 */
+		setVisitorInfo: (options: VisitorInfoOptions) => void;
 	}
 
 	const RNZendeskChatModule: RNZendeskChatModuleImpl;
