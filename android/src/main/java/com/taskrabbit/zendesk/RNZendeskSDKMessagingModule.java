310f56091837b4fedc64a2211bbd6ea6560deee0
Radu Popovici <popovici.radu@gmail.com>
2025-05-01 19:00:41 +0300

    implement login and logout

--- android/src/main/java/com/taskrabbit/zendesk/RNZendeskSDKMessagingModule.java
+++ android/src/main/java/com/taskrabbit/zendesk/RNZendeskSDKMessagingModule.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.Application;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -12,41 +14,78 @@ import com.facebook.react.bridge.Promise;
 
 import java.lang.String;
 
+
+import kotlin.Unit;
+import zendesk.android.FailureCallback;
+import zendesk.android.SuccessCallback;
 import zendesk.android.Zendesk;
+import zendesk.android.ZendeskUser;
 import zendesk.messaging.android.DefaultMessagingFactory;
 import zendesk.logger.Logger;
 
 public class RNZendeskSDKMessagingModule extends ReactContextBaseJavaModule {
-		private static final String TAG = "[RNZendeskSDKMessagingModule]";
-		private Application applicationContext;
-		private ReactContext reactContext;
+	private static final String TAG = "[RNZendeskSDKMessagingModule]";
+	private Application applicationContext;
+	private ReactContext reactContext;
 
-    public RNZendeskSDKMessagingModule(ReactApplicationContext reactContext) {
-			super(reactContext);
-			this.reactContext = reactContext;
-			this.applicationContext = (Application) reactContext.getApplicationContext();
-    }
+	public RNZendeskSDKMessagingModule(ReactApplicationContext reactContext) {
+		super(reactContext);
+		this.reactContext = reactContext;
+		this.applicationContext = (Application) reactContext.getApplicationContext();
+	}
 
-    @Override
-    public String getName() {
-			return "RNZendeskSDKMessagingModule";
-    }
+	@Override
+	public String getName() {
+		return "RNZendeskSDKMessagingModule";
+	}
 
-    @ReactMethod
-    public void initialize(String channelKey, Promise promise) {
-			Zendesk.initialize(
-				this.applicationContext,
-				channelKey,
-				zendesk -> {
-					Log.i(TAG, "Initialization successful");
-					promise.resolve(true);
-				},
-				error -> {
-					Log.e(TAG, "Messaging failed to initialize", error);
-					promise.reject("INIT_ERROR", error.getLocalizedMessage(), error);
-				},
-				new DefaultMessagingFactory());
-    }
+	@ReactMethod
+	public void initialize(String channelKey, Promise promise) {
+		Zendesk.initialize(
+			this.applicationContext,
+			channelKey,
+			zendesk -> {
+				Log.i(TAG, "Initialization successful");
+				promise.resolve(true);
+			},
+			error -> {
+				Log.e(TAG, "Messaging failed to initialize", error);
+				promise.reject("INIT_ERROR", error.getLocalizedMessage(), error);
+			},
+			new DefaultMessagingFactory());
+	}
+
+	@ReactMethod
+	public void loginUser(String jwt, Promise promise) {
+		Zendesk.getInstance().loginUser(jwt,
+			new SuccessCallback<ZendeskUser>() {
+				@Override
+				public void onSuccess(ZendeskUser user) {
+					promise.resolve(user);
+				}},
+			new FailureCallback<Throwable>() {
+				@Override
+				public void onFailure(@NonNull Throwable error) {
+					promise.reject("LOGIN_ERROR", error.getLocalizedMessage(), error);
+				}
+		});
+	}
+
+	@ReactMethod
+	public void logoutUser(Promise promise) {
+		Zendesk.getInstance().logoutUser(
+			new SuccessCallback<Unit>() {
+				@Override
+				public void onSuccess(Unit value) {
+					promise.resolve(value);
+				}},
+			new FailureCallback<Throwable>() {
+				@Override
+				public void onFailure(@NonNull Throwable error) {
+					promise.reject("LOGOUT_ERROR", error.getLocalizedMessage(), error);
+				}
+			});
+	}
 
 	@ReactMethod
 	public void showMessaging() {
