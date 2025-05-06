0b8fc8202722ca4d1c8cce7796142ac4c67b3cfc
Radu Popovici <popovici.radu@gmail.com>
2025-05-06 14:20:16 +0300

    add swift module

---
+++ ios/RNZendeskSDKMessagingModule.swift
@@ -0,0 +1,9 @@
+//
+//  RNZendeskSDKMessagingModule.swift
+//  RNZendeskSDKMessaging
+//
+//  Created by Radu Popovici on 06.05.2025.
+//  Copyright © 2025 TaskRabbit. All rights reserved.
+//
+
+import Foundation
