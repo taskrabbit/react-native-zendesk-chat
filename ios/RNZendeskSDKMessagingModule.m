874274d21e331d67a8db3906e70ec84727f0eb80
Radu Popovici <popovici.radu@gmail.com>
2025-05-06 19:10:23 +0300

    convert module to swift

--- ios/RNZendeskSDKMessagingModule.m
+++ ios/RNZendeskSDKMessagingModule.m
@@ -5,207 +5,39 @@
 //  Created by Jean-Richard Lai on 11/23/15.
 //
 
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-#import "RNZendeskSDKMessagingModule.h"
+@interface RCT_EXTERN_MODULE(RNZendeskSDKMessagingModule, RCTEventEmitter)
 
-#import <React/RCTUtils.h>
-#import <React/RCTConvert.h>
-
-#import <ZendeskSDK/ZendeskSDK.h>
-#import <ZendeskSDKMessaging/ZendeskSDKMessaging.h>
-#import <ZendeskSDKLogger/ZendeskSDKLogger.h>
-
-@interface RNZendeskSDKMessagingModule ()
-@end
-
-@implementation RNZendeskSDKMessagingModule
-
-NSArray *SUPPORTED_EVENTS = @[
-    /// Invoked when there is a change to the current total number of unread messages.
-    @"ZDKZendeskEventUnreadMessageCountChanged",
-    /// Invoked when a rest call fails for authentication reasons.
-    @"ZDKZendeskEventAuthenticationFailed",
-    /// Invoked when a conversation has been added.
-    @"ZDKZendeskEventConversationAdded",
-    /// The SDK <code>ConnectionStatus</code> has changed due to an action or another event.
-    @"ZDKZendeskEventConnectionStatusChanged",
-    /// Invoked when a message fails to be sent.
-    @"ZDKZendeskEventSendMessageFailed",
-    /// Invoked when the conversation screen is opened.
-    @"ZDKZendeskEventConversationOpened",
-    /// Invoked when the conversation is started on the device.
-    @"ZDKZendeskEventConversationStarted",
-    /// Invoked when the messages shown to the user are updated.
-    @"ZDKZendeskEventMessagesShown"
-];
-
-
-RCT_EXPORT_MODULE(RNZendeskSDKMessagingModule);
-
-- (NSArray<NSString *> *)supportedEvents {
-    return SUPPORTED_EVENTS;
-}
-
-RCT_EXPORT_METHOD(initialize: (NSString *)channelKey
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [Zendesk initializeWithChannelKey:channelKey messagingFactory:[[ZDKDefaultMessagingFactory alloc] init] completionHandler:^(Zendesk * _Nullable zendesk, NSError * _Nullable error) {
-            if (error != nil) {
-                NSLog(@"Zendesk did not initialize.\nError: %@", error.localizedDescription);
-                reject(@(error.code).stringValue, error.localizedDescription, error);
-                return;
-            }
-            resolve(@(YES));
-        }];
-    });
-}
-
-RCT_EXPORT_METHOD(startObservingEvents) {
-    // Get your Zendesk instance:
-    Zendesk *instance = [Zendesk instance];
-    
-    // To add an event observer to your Zendesk instance:
-    [instance addEventObserver:self :^(enum ZDKZendeskEvent event, id _Nullable value) {
-        switch (event) {
-            case ZDKZendeskEventUnreadMessageCountChanged:
-                [self sendEventWithName:@"ZDKZendeskEventUnreadMessageCountChanged" body:value];
-            case ZDKZendeskEventAuthenticationFailed:
-                [self sendEventWithName:@"ZDKZendeskEventAuthenticationFailed" body:value];
-            case ZDKZendeskEventConversationAdded:
-                [self sendEventWithName:@"ZDKZendeskEventConversationAdded" body:value];
-            case ZDKZendeskEventConnectionStatusChanged:
-                [self sendEventWithName:@"ZDKZendeskEventConnectionStatusChanged" body:value];
-            case ZDKZendeskEventSendMessageFailed:
-                [self sendEventWithName:@"ZDKZendeskEventSendMessageFailed" body:value];
-            case ZDKZendeskEventConversationOpened:
-                [self sendEventWithName:@"ZDKZendeskEventConversationOpened" body:value];
-            case ZDKZendeskEventConversationStarted:
-                [self sendEventWithName:@"ZDKZendeskEventConversationStarted" body:value];
-            case ZDKZendeskEventMessagesShown:
-                [self sendEventWithName:@"ZDKZendeskEventMessagesShown" body:value];
-            default:
-                break;
-        }
-    }];
-}
-
-RCT_EXPORT_METHOD(stopObservingEvents) {
-    // Get your Zendesk instance:
-    Zendesk *instance = [Zendesk instance];
-    
-    // To remove an event observer from your Zendesk instance:
-    [instance removeEventObserver:self];
-}
-
-RCT_EXPORT_METHOD(loginUser: (NSString *)jwt
+RCT_EXTERN_METHOD(initialize: (NSString *)channelKey
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        Zendesk *instance = [Zendesk instance];
-        [instance loginUserWith:jwt completionHandler:^(ZDKZendeskUser * _Nullable user, NSError * _Nullable error) {
-            if (error != nil) {
-                NSLog(@"Zendesk can't login.\nError: %@", error.localizedDescription);
-                reject(@(error.code).stringValue, error.localizedDescription, error);
-                return;
-            }
-            // login successful
-            resolve(user);
-        }];
-    });
-}
 
-RCT_EXPORT_METHOD(logoutUser: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        Zendesk *instance = [Zendesk instance];
-        [instance logoutUserWithCompletionHandler:^(NSError * _Nullable error) {
-            if (error != nil) {
-                NSLog(@"Zendesk can't logout.\nError: %@", error.localizedDescription);
-                reject(@(error.code).stringValue, error.localizedDescription, error);
-                return;
-            }
-            // logout successful
-            resolve(@(YES));
-        }];
-    });
-}
+RCT_EXTERN_METHOD(loginUser: (NSString *)jwt
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXPORT_METHOD(showMessaging) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIViewController *rootViewController = RCTPresentedViewController();
-        UIViewController *messagingViewController = [Zendesk.instance.messaging messagingViewController];
-        
-        if (rootViewController != nil && messagingViewController != nil) {
-            if ([rootViewController isMemberOfClass: [messagingViewController class]]) {
-                // exit early if the ZenDesk chat is already presented
-                return;
-            }
-            
-            [rootViewController showViewController:messagingViewController sender:self];
-        }
-    });
-}
+RCT_EXTERN_METHOD(logoutUser: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXPORT_METHOD(hideMessaging)
-{
-  dispatch_async(dispatch_get_main_queue(), ^{
-      UIViewController *presentedViewController = RCTPresentedViewController();
-      UIViewController *messagingViewController = [Zendesk.instance.messaging messagingViewController];
+RCT_EXTERN_METHOD(showMessaging)
 
-      if (presentedViewController != nil &&
-          messagingViewController != nil &&
-          [presentedViewController isMemberOfClass: [messagingViewController class]]) {
-          // dismiss only if the presented view controller is the ZenDesk chat modal
-          [presentedViewController dismissViewControllerAnimated:YES completion:nil];
-      }
-  });
-}
+RCT_EXTERN_METHOD(hideMessaging)
 
-RCT_EXPORT_METHOD(getUnreadMessageCount:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        // Get the instance of messaging
-        Zendesk *instance = [Zendesk instance];
-        // Get the unread count
-        NSNumber *unreadCount = @(instance.messaging.getUnreadMessageCount);
-        resolve(unreadCount);
-    });
-}
+RCT_EXTERN_METHOD(getUnreadMessageCount:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXPORT_METHOD(sendPageViewEvent:(NSString *)pageTitle
+RCT_EXTERN_METHOD(sendPageViewEvent:(NSString *)pageTitle
                   withUrl:(NSString *)url
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        Zendesk *instance = [Zendesk instance];
-        // Create a PageView
-        ZDKPageView *pageView = [[ZDKPageView alloc] initWithPageTitle:pageTitle url:url];
-        [instance sendPageViewEvent:pageView completionHandler:^(NSError *error) {
-            if (error != nil) {
-                NSLog(@"Zendesk can't send PageView event.\nError: %@", error.localizedDescription);
-                reject(@(error.code).stringValue, error.localizedDescription, error);
-                return;
-            }
-            // event sent successfully
-            resolve(@(YES));
-        }];
-    });
-}
 
-RCT_EXPORT_METHOD(invalidate:(BOOL)clearStorage)
-{
-  dispatch_async(dispatch_get_main_queue(), ^{
-      [Zendesk invalidateWithClearStorage:clearStorage];
-  });
-}
+RCT_EXTERN_METHOD(startObservingEvents)
 
-RCT_EXPORT_METHOD(setLoggable:(BOOL)enable location:(NSInteger)logLevel)
-{
-    ZDKLogger.enabled = enable;
-    ZDKLogger.level = logLevel;
-}
+RCT_EXTERN_METHOD(stopObservingEvents)
+
+RCT_EXTERN_METHOD(invalidate:(BOOL)clearStorage)
+
+RCT_EXTERN_METHOD(setLoggable:(BOOL)enable level:(NSInteger)logLevel)
 
 @end
--- ios/RNZendeskSDKMessagingModule.swift
+++ ios/RNZendeskSDKMessagingModule.swift
@@ -9,7 +9,208 @@
 import Foundation
 import React
 
-@objc(RNZendeskSDKMessagingModule)
-class RNZendeskSDKMessagingModule: NSObject {
+import ZendeskSDKMessaging
+import ZendeskSDK
+import ZendeskSDKLogger
 
+@objc(RNZendeskSDKMessagingModule)
+class RNZendeskSDKMessagingModule: RCTEventEmitter {
+
+    private let SUPPORTED_EVENTS: [String] = [
+        /// Invoked when there is a change to the current total number of unread messages.
+        "ZDKZendeskEventUnreadMessageCountChanged",
+        /// Invoked when a rest call fails for authentication reasons.
+        "ZDKZendeskEventAuthenticationFailed",
+        /// Invoked when a conversation has been added.
+        "ZDKZendeskEventConversationAdded",
+        /// The SDK <code>ConnectionStatus</code> has changed due to an action or another event.
+        "ZDKZendeskEventConnectionStatusChanged",
+        /// Invoked when a message fails to be sent.
+        "ZDKZendeskEventSendMessageFailed",
+        /// Invoked when the conversation screen is opened.
+        "ZDKZendeskEventConversationOpened",
+        /// Invoked when the conversation is started on the device.
+        "ZDKZendeskEventConversationStarted",
+        /// Invoked when the messages shown to the user are updated.
+        "ZDKZendeskEventMessagesShown"
+    ];
+
+    override func supportedEvents() -> [String] {
+      return SUPPORTED_EVENTS
+    }
+    
+    // MARK: - React Native Functions
+    
+    @objc(initialize:resolver:rejecter:)
+    func initialize(channelKey: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        DispatchQueue.main.async {
+            Zendesk.initialize(withChannelKey: channelKey, messagingFactory: DefaultMessagingFactory()) {
+                result in
+                if case .success(_) = result {
+                    resolve(true)
+                }
+                if case let .failure(error) = result {
+                    print("Zendesk did not initialize.\nError: \(error.localizedDescription)")
+                    reject("INIT_ERROR", error.localizedDescription, error)
+                }
+            }
+        }
+    }
+    
+    @objc(loginUser:resolver:rejecter:)
+    func loginUser(jwt: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        DispatchQueue.main.async {
+            Zendesk.instance?.loginUser(with: jwt) { result in
+                switch result {
+                case .success(let user):
+                    resolve(user)
+                case .failure(let error):
+                    reject("LOGIN_ERROR", error.localizedDescription, error)
+                }
+            }
+        }
+    }
+    
+    @objc(logoutUser:rejecter:)
+    func logoutUser(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        DispatchQueue.main.async {
+            Zendesk.instance?.logoutUser { result in
+                switch result {
+                case .success:
+                    resolve(true)
+                case .failure(let error):
+                    reject("LOGOUT_ERROR", error.localizedDescription, error)
+                }
+            }
+        }
+    }
+    
+    @objc(showMessaging)
+    func showMessaging() {
+        DispatchQueue.main.async {
+            guard let rootViewController = RCTPresentedViewController() else {
+                print("No presented view controller found")
+                return
+            }
+            
+            guard let messagingViewController = Zendesk.instance?.messaging?.messagingViewController() else {
+                print("Zendesk is not initialized")
+                return
+            }
+            
+            if (type(of: rootViewController) == type(of: messagingViewController) ) {
+                // exit early if the ZenDesk chat is already presented
+                return;
+            }
+            
+            rootViewController.show(messagingViewController, sender:self);
+        }
+    }
+    
+    @objc(hideMessaging)
+    func hideMessaging() {
+        DispatchQueue.main.async {
+            guard let presentedViewController = RCTPresentedViewController() else {
+                print("No presented view controller found")
+                return
+            }
+            
+            guard let messagingViewController = Zendesk.instance?.messaging?.messagingViewController() else {
+                print("Zendesk is not initialized")
+                return
+            }
+
+            if (type(of: presentedViewController) == type(of: messagingViewController) ) {
+                // dismiss only if the presented view controller is the ZendeskSDKMessaging.ConversationViewController
+                presentedViewController.dismiss(animated: true, completion: nil);
+            }
+        }
+    }
+    
+    @objc(getUnreadMessageCount:rejecter:)
+    func getUnreadMessageCount(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        DispatchQueue.main.async {
+            let unreadMessageCount = Zendesk.instance?.messaging?.getUnreadMessageCount()
+            resolve(unreadMessageCount)
+        }
+    }
+    
+    @objc(sendPageViewEvent:withUrl:resolver:rejecter:)
+    func sendPageViewEvent(pageTitle: String, withUrl url: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        DispatchQueue.main.async {
+            // Create a `PageView` object
+            let pageView = PageView(pageTitle: pageTitle, url: url)
+            Zendesk.instance?.sendPageViewEvent(pageView) { result in
+                switch result {
+                case .success:
+                    resolve(true)
+                case .failure(let error):
+                    reject("PAGEVIEW_ERROR", error.localizedDescription, error)
+                }
+            }
+        }
+    }
+    
+    @objc(startObservingEvents)
+    func startObservingEvents() {
+        DispatchQueue.main.async {
+            Zendesk.instance?.addEventObserver(self) { event in
+                switch event {
+                case .unreadMessageCountChanged(let unreadCount):
+                    self.sendEvent(withName: "ZDKZendeskEventUnreadMessageCountChanged", body: unreadCount)
+                case .authenticationFailed(let error as NSError):
+                    self.sendEvent(withName: "ZDKZendeskEventAuthenticationFailed", body: error)
+                case .conversationAdded(conversationId: let conversationId):
+                    self.sendEvent(withName: "ZDKZendeskEventConversationAdded", body: conversationId)
+                case .connectionStatusChanged(connectionStatus: let connectionStatus):
+                    self.sendEvent(withName: "ZDKZendeskEventConnectionStatusChanged", body: connectionStatus)
+                case .sendMessageFailed(let error as NSError):
+                    self.sendEvent(withName: "ZDKZendeskEventSendMessageFailed", body: error)
+                case .conversationOpened(id: let id, timestamp: let timestamp, conversationId: let conversationId):
+                    self.sendEvent(withName: "ZDKZendeskEventConversationOpened", body: [
+                        "id": id,
+                        "timestamp": timestamp,
+                        "conversationId": conversationId ?? ""
+                    ])
+                case .conversationStarted(id: let id, timestamp: let timestamp, conversationId: let conversationId):
+                    self.sendEvent(withName: "ZDKZendeskEventConversationStarted", body: [
+                        "id": id,
+                        "timestamp": timestamp,
+                        "conversationId": conversationId
+                    ])
+                case .messagesShown(id: let id, timestamp: let timestamp, conversationId: let conversationId, messages: let messages):
+                    self.sendEvent(withName: "ZDKZendeskEventMessagesShown", body: [
+                        "id": id,
+                        "timestamp": timestamp,
+                        "conversationId": conversationId,
+                        "messages": messages
+                    ])
+                @unknown default:
+                    break
+                }
+            }
+        }
+    }
+    
+    @objc(stopObservingEvents)
+    func stopObservingEvents() {
+        DispatchQueue.main.async {
+            Zendesk.instance?.removeEventObserver(self)
+        }
+    }
+    
+    @objc(invalidate:)
+    func invalidate(clearStorage: Bool) {
+        DispatchQueue.main.async {
+            Zendesk.invalidate(clearStorage)
+        }
+    }
+    
+    @objc(setLoggable:level:)
+    func setLoggable(enable: Bool, level: LogLevel) {
+        DispatchQueue.main.async {
+            Logger.enabled = enable
+            Logger.level = level
+        }
+    }
 }
