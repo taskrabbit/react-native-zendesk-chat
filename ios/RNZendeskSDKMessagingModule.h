ee5702fd16db2f0e175b1e9bb8602be1cdca88be
Radu Popovici <popovici.radu@gmail.com>
2025-05-06 14:38:08 +0300

    remove objc header

--- ios/RNZendeskSDKMessagingModule.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import <React/RCTEventEmitter.h>
-#import <React/RCTBridgeModule.h>
-
-@interface RNZendeskSDKMessagingModule : RCTEventEmitter<RCTBridgeModule>
-
-@end
